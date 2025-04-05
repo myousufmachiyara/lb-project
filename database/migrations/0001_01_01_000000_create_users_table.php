71f4e4ea3a827338e81b3ce5b972d5f782639c28
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2025-04-05 17:00:43 +0500

    fix

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,12 +28,12 @@ return new class extends Migration
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->string('id', 255)->charset('utf8')->collation('utf8_unicode_ci')->primary();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->integer('last_activity');
         });
     }
 
