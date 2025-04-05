f6614625e30fd16312a043082191abb8e76f16b3
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2025-04-05 17:12:56 +0500

    fixes

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->string('key', 255)->charset('utf8')->collation('utf8_unicode_ci')->primary();
             $table->string('owner');
             $table->integer('expiration');
         });
