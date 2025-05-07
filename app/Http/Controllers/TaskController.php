971a9fa892226b791b90f2a1ba9da1bf2fde4081
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2025-05-07 23:47:15 +0500

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use App\Models\TaskCategory;
+use App\Models\ProjectStatus;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -14,9 +15,11 @@ class TaskController extends Controller
     public function index()
     {
         try {
-            $tasks = Task::with(['project', 'status', 'category', 'project.attachments'])->get();
+            $tasks = Task::with(['project', 'project.attachments'])->get();
             $category = TaskCategory::all(); // Fetch all categories
-            return view('tasks.index', compact('tasks', 'category'));
+            $status = ProjectStatus::all(); // Fetch all categories
+
+            return view('tasks.index', compact('tasks', 'category', 'status'));
         } catch (\Exception $e) {
             Log::error('Error fetching tasks: ' . $e->getMessage());
             return redirect()->back()->with('error', 'Failed to retrieve tasks.');
